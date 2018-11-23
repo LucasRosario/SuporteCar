f39c6db83cf9e314034d1c5195b2d386605cba15
Lucas Rosario <lucasrosario1@hotmail.com>
2018-11-23 15:05:54 -0300

    Tela de Senha

---
+++ js/login.js
@@ -0,0 +1,23 @@
+var attempt = 3; // Variable to count number of attempts.
+
+
+// Below function Executes on click of login button.
+function validate(){
+	var username = document.getElementById("inputEmail").value;
+	var password = document.getElementById("inputPassword").value;
+		if ( username == "lucasrosario@car.ba.gov.br" && password == "car123"){
+			alert ("Login successfully");
+			window.location = "http://www.car.ba.gov.br"; // Redirecting to other page.
+			return false;
+		}else{
+			attempt --;// Decrementing by one.
+			alert("You have left "+attempt+" attempt");
+			// Disabling fields after 3 attempts.
+			if( attempt == 0){
+				document.getElementById("username").disabled = true;
+				document.getElementById("password").disabled = true;
+				document.getElementById("submit").disabled = true;
+				return false;
+			}
+		}
+}
