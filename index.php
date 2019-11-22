b7aefcc136480e21f4992224815e81f057f2aeba
Lucas Rosario <lucasrosario1@hotmail.com>
2019-11-21 23:15:10 -0300

    Configuração do DB, autoloader e Tpl

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Main\DB\Sql();
+
+	$results = $sql->select("SELECT * FROM tb_usuario");
+
+	echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
